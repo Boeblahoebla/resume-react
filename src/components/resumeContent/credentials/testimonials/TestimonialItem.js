582c7638332347547be1fc23ad20d2d508fcfe05
Dzengiz Tafa <dzengiz.tafa@ateljeevzw.be>
2019-06-20 09:35:27 +0200

    The paragraph in the testimonial item is now clickable. It will expand or collapse to show the full or partial text

--- src/components/resumeContent/credentials/testimonials/TestimonialItem.js
+++ src/components/resumeContent/credentials/testimonials/TestimonialItem.js
@@ -2,7 +2,7 @@
 //////////
 
 // Dependencies
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 // Media
 import patrick from '../../../../images/patrick.png';
@@ -14,6 +14,14 @@ import michael from '../../../../images/michael.png'
 
 const TestimonialItem = ({ data }) => {
 
+    // State
+    const [paragraphContent, setParagraphContent] = useState(data.testimonial);
+
+    // When the component receives new props, reset the paragraphContent
+    useEffect(() => {
+        setParagraphContent(data.testimonial.substring(0, 100) + ' ...');
+    }, [data.testimonial]);
+
     // Find the right avatar
     let avatar;
 
@@ -21,14 +29,23 @@ const TestimonialItem = ({ data }) => {
         avatar = patrick
     } else {
         avatar = michael
+    }
+
+    // Handler to expand or contract the paragraph
+    const paragraphContentHandler = () => {
+        if (paragraphContent.indexOf(' ...') > -1) {
+            setParagraphContent(data.testimonial)
+        } else {
+            setParagraphContent(data.testimonial.substring(0, 100) + ' ...');
+        }
     };
 
     return (
         <div className="credentials__section credentials__section--testimonial">
             <h3>Patrick Moesick <small className="text-muted">- AteljeeVZW</small></h3>
             <div className="credentials__section__item mb-2">
-                <p className="mb-0 lang" data-key="test1-text">
-                    {data.testimonial}
+                <p className="mb-0 lang" data-key="test1-text" onClick={() => paragraphContentHandler()}>
+                    {paragraphContent}
                 </p>
             </div>
             <div className="credentials__section__avatar">
